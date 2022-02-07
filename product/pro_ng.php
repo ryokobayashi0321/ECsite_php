c1ba3e8b02d01ae02fce786824fc67b08dd5ac8d
ryo <if14germany1248@icloud.com>
2022-02-07 17:09:28 +0900

    商品一覧、ページ遷移先指定画面

---
+++ product/pro_ng.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+session_regenerate_id(true);
+if (isset($_SESSION['login']) === false) {
+    echo 'ログインしていません。' . PHP_EOL;
+    echo '<a href="staff_login.html">ログイン画面へ</a>';
+    exit();
+} else {
+    echo $_SESSION['name'] . 'さんログイン中' . PHP_EOL;
+    echo '<br><br>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品選択NG</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+
+    商品を選択してください<br><br>
+    <a href="pro_list.php">商品一覧へ</a>
+</body>
+</html>
